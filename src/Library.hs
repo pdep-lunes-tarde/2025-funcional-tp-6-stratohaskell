85fe53fd63f5eb9f37893922ec93d0b1e24ae1ad
AgusArias11 <agustinariascovnatsky@gmail.com>
2025-05-06 18:26:05 -0300

    parte 1 y 2 terminadas

--- src/Library.hs
+++ src/Library.hs
@@ -2,7 +2,7 @@ module Library where
 import PdePreludat
 
 data Ingrediente =
-    Carne | Pan | Panceta | Cheddar | Pollo | Curry | QuesoDeAlmendras
+    Carne | Pan | Panceta | Cheddar | Pollo | Curry | QuesoDeAlmendras | Papas | PatiVegano
     deriving (Eq, Show)
 
 precioIngrediente Carne = 20
@@ -12,9 +12,50 @@ precioIngrediente Cheddar = 10
 precioIngrediente Pollo =  10
 precioIngrediente Curry = 5
 precioIngrediente QuesoDeAlmendras = 15
+precioIngrediente Papas = 10
+precioIngrediente PatiVegano = 10
 
 data Hamburguesa = Hamburguesa {
     precioBase :: Number,
     ingredientes :: [Ingrediente]
 } deriving (Eq, Show)
+--parte 1
+cuartoDeLibra = Hamburguesa {precioBase =20, ingredientes = [Pan,Carne,Cheddar,Pan]}
+hamburguesaDePollo = Hamburguesa {precioBase =30,ingredientes=[Pan,Pollo,Pan]}
+
+hamburguesaMixta =Hamburguesa {precioBase =20, ingredientes = [Pan,Carne,Pollo,Cheddar,Pan]}
+
+tieneCarne:: Hamburguesa -> Bool
+tieneCarne ham = any (==Carne)  (ingredientes ham)
+tienePollo::  Hamburguesa -> Bool
+tienePollo ham = any (==Pollo)  (ingredientes ham)
+
+precioFinal:: Hamburguesa ->Number
+precioFinal ham = precioBase ham + sum (map precioIngrediente (ingredientes ham))
+
+agrandar:: Hamburguesa->Hamburguesa
+agrandar hamburguesa
+    | tienePatiVegano
+    | tieneCarne hamburguesa = agregarIngrediente Carne hamburguesa
+    | tienePollo hamburguesa =agregarIngrediente Pollo hamburguesa
+
+agregarIngrediente :: Ingrediente ->Hamburguesa->Hamburguesa
+agregarIngrediente ingrediente ham = ham{ingredientes= ingrediente: ingredientes ham}
+
+descuento:: Number->Hamburguesa->Hamburguesa
+descuento porcentaje ham = ham {precioBase = precioBase ham - precioBase ham* (porcentaje/100)}
+
+pdepBurger :: Hamburguesa
+pdepBurger = (agregarIngrediente Panceta. agregarIngrediente Cheddar . agrandar. agrandar. descuento 20) cuartoDeLibra
+
+--parte 2
+
+dobleCuarto :: Hamburguesa
+dobleCuarto = (agrandar . agregarIngrediente Cheddar) cuartoDeLibra
+
+bigPdep :: Hamburguesa
+bigPdep = agregarIngrediente Curry dobleCuarto
+
+delDia :: Hamburguesa->Hamburguesa
+delDia ham = (agregarIngrediente Papas . descuento 30) ham
 
--- src/Spec.hs
+++ src/Spec.hs
@@ -6,6 +6,41 @@ import Control.Exception (evaluate)
 
 correrTests :: IO ()
 correrTests = hspec $ do
+    --parte 1
     describe "TP 5" $ do
         it "test de prueba" $ do
             2 + 2 `shouldBe` 4
+    describe "agrandar" $ do
+        it "Dada una hamburguesa con carne, devuelve la hamburguesa con una carne mas, es decir 2 carnes" $ do
+            agrandar cuartoDeLibra `shouldBe` Hamburguesa {precioBase =20, ingredientes = [Carne,Pan,Carne,Cheddar,Pan]}
+        it "Dada una hamburguesa con pollo, devuelve la hamburguesa con un pollo mas, es decir 2 pollos" $ do
+            agrandar hamburguesaDePollo `shouldBe` Hamburguesa {precioBase =30,ingredientes=[Pollo,Pan,Pollo,Pan]}
+        it "Si se recibe una hamburguesa mixta (con carne y pollo), se devuelve la hamburguesa con una carne mas" $ do
+            agrandar hamburguesaMixta `shouldBe` Hamburguesa {precioBase =20, ingredientes = [Carne,Pan,Carne,Pollo,Cheddar,Pan]}
+
+    describe "agregarIngrediente" $ do
+        it "dada un ingrediente y una hamburguesa, se devuelve la hamburguesa con el ingrediente agregado" $ do
+            agregarIngrediente  Panceta cuartoDeLibra `shouldBe` Hamburguesa {precioBase =20, ingredientes = [Panceta,Pan,Carne,Cheddar,Pan]}
+
+    describe "descuento" $ do
+        it "dado un porcentaje y una Hamburguesa, devuelve la hamburguesa con el precio base reducido segun el porcentaje" $ do
+            descuento 25 cuartoDeLibra `shouldBe` Hamburguesa {precioBase =15, ingredientes = [Pan,Carne,Cheddar,Pan]}
+    describe "pdepBurger" $ do
+        it "La pdepBurger debe tener como precio final 110" $ do
+            precioFinal pdepBurger `shouldBe` 110
+    
+    --parte 2
+
+    describe "dobleCuarto " $ do
+        it "el precioFinal de doble cuarto debería ser 84" $ do
+            precioFinal dobleCuarto `shouldBe` 84
+            
+    describe "bigPdep " $ do
+        it "el precioFinal de bigPdep debería ser 89" $ do
+            precioFinal bigPdep `shouldBe` 89
+    
+    describe "delDia " $ do
+        it "dada una hamburguesa se le tiene que agregar papas y un 30 porciento de descuento en el precio base" $ do
+            precioFinal (delDia dobleCuarto) `shouldBe` 88
+        
+     
